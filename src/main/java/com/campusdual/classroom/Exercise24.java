9ac3f63fcc72c258b5399e6cd5c8f48e87964930
JuanDAW37 <jymtr1968@gmail.com>
2024-10-03 11:20:23 +0200

    Exercicio 24 listo

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -1,18 +1,34 @@
 package com.campusdual.classroom;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise24 {
-    public static Queue<String> createQueue() {
 
+    public static Queue<String> cola = new LinkedList<>();
+
+    public static Queue<String> createQueue() {
+        cola.add("Smith");
+        cola.add("Montessori");
+        cola.add("Peralta");
+        cola.add("House");
+        Iterator<String> it =cola.iterator();
+        while(it.hasNext()) {
+            String element = it.next();
+            System.out.println(element);
+        }
+        return cola;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
-
+        cola = createQueue();
+        printAndEmptyQueue(cola);
     }
 
 }
